b1dd1646ab7f5355d88bc1fac2239a9400359a67
aalhikamlearningcenter-dot <aalhikamlearningcenter@gmail.com>
2026-08-02 07:15:28 +0100

    Update database.py

--- database.py
+++ database.py
@@ -1,11 +1,149 @@
-students = {}
+import sqlite3
 
-def add_student(user_id, data):
-    students[user_id] = data
+DATABASE_NAME = "alhikam.db"
 
-def get_student(user_id):
-    return students.get(user_id)
 
-def payment_verified(user_id):
-    if user_id in students:
-        students[user_id]["verified"] = True
+def get_connection():
+    conn = sqlite3.connect(DATABASE_NAME)
+    conn.row_factory = sqlite3.Row
+    return conn
+
+
+def initialize_database():
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS students (
+
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+
+        payment_token TEXT UNIQUE,
+
+        tx_ref TEXT UNIQUE,
+
+        full_name TEXT,
+
+        phone TEXT,
+
+        email TEXT,
+
+        course TEXT,
+
+        telegram_id TEXT,
+
+        telegram_username TEXT,
+
+        telegram_name TEXT,
+
+        payment_plan TEXT,
+
+        amount_paid REAL,
+
+        payment_status TEXT,
+
+        registration_completed INTEGER DEFAULT 0,
+
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+
+    )
+    """)
+
+    conn.commit()
+    conn.close()
+
+
+def add_student(data):
+
+    conn = get_connection()
+
+    cursor = conn.cursor()
+
+    cursor.execute("""
+
+    INSERT INTO students(
+
+        payment_token,
+
+        tx_ref,
+
+        full_name,
+
+        phone,
+
+        email,
+
+        course,
+
+        telegram_id,
+
+        telegram_username,
+
+        telegram_name,
+
+        payment_plan,
+
+        amount_paid,
+
+        payment_status,
+
+        registration_completed
+
+    )
+
+    VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?)
+
+    """, (
+
+        data["payment_token"],
+
+        data["tx_ref"],
+
+        data["full_name"],
+
+        data["phone"],
+
+        data["email"],
+
+        data["course"],
+
+        data.get("telegram_id"),
+
+        data.get("telegram_username"),
+
+        data.get("telegram_name"),
+
+        data["payment_plan"],
+
+        data["amount_paid"],
+
+        data["payment_status"],
+
+        data["registration_completed"]
+
+    ))
+
+    conn.commit()
+
+    conn.close()
+
+
+def get_student(payment_token):
+
+    conn = get_connection()
+
+    cursor = conn.cursor()
+
+    cursor.execute(
+
+        "SELECT * FROM students WHERE payment_token=?",
+
+        (payment_token,)
+
+    )
+
+    student = cursor.fetchone()
+
+    conn.close()
+
+    return student
