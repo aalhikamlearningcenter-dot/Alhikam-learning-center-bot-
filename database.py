9b25381915e77ffeea3531c3c1f049cda07c4d18
aalhikamlearningcenter-dot <aalhikamlearningcenter@gmail.com>
2026-08-02 13:51:48 +0100

    Update database.py

--- database.py
+++ database.py
@@ -147,3 +147,38 @@ def get_student(payment_token):
     conn.close()
 
     return student
+
+def update_student(payment_token, data):
+
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute(
+        """
+        UPDATE students
+        SET
+            full_name=?,
+            phone=?,
+            email=?,
+            course=?,
+            telegram_id=?,
+            telegram_username=?,
+            telegram_name=?,
+            registration_completed=?
+        WHERE payment_token=?
+        """,
+        (
+            data["full_name"],
+            data["phone"],
+            data["email"],
+            data["course"],
+            data.get("telegram_id"),
+            data.get("telegram_username"),
+            data.get("telegram_name"),
+            1,
+            payment_token,
+        ),
+    )
+
+    conn.commit()
+    conn.close()
