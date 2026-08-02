49ad738099d9d4c59ed03357aad270bcb8fbf93d
aalhikamlearningcenter-dot <aalhikamlearningcenter@gmail.com>
2026-08-02 07:26:52 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,28 @@
-import os
+# ==========================================
+# APP
+# ==========================================
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-FLW_SECRET_KEY = os.getenv("FLW_SECRET_KEY")
-PAYMENT_LINK = os.getenv("PAYMENT_LINK")
+APP_NAME = "ALHIKAM Learning Center"
+
+APP_URL = "https://precious-trust-production-956b.up.railway.app"
+
+BOT_USERNAME = "Alhikamcenterbot"
+
+
+# ==========================================
+# TELEGRAM CHANNEL IDS
+# ==========================================
+
+MAIN_GROUP_ID = -1004384506380
+ANNOUNCEMENT_CHANNEL_ID = -1004315707986
+
+SCIENCE_FACULTY_ID = -1004479887604
+PHYSICS_ID = -1004467391688
+CHEMISTRY_ID = -1003575115831
+BIOLOGY_ID = -1004412247385
+MATHEMATICS_ID = -1004480230539
+AGRICULTURAL_SCIENCE_ID = -1004398599335
+GEOGRAPHY_ID = -1003901130871
+
+ARTS_FACULTY_ID = -1004314659728
+COMMERCIAL_FACULTY_ID = -1003967146846
