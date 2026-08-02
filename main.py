23698ba8ea550358165bfb26e84532ff4770ee37
aalhikamlearningcenter-dot <aalhikamlearningcenter@gmail.com>
2026-08-02 16:41:00 +0100

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,10 @@ import os
 import uuid
 import requests
 from registration import registration_page
-from telegram_service import send_message
+from telegram_service import (
+    send_message,
+    send_welcome_message,
+)
 from flask import (
     Flask,
     request,
