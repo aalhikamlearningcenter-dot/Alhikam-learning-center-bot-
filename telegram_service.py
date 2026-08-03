67f461be8ed2a2291a65697246ee50c7aacf44c5
aalhikamlearningcenter-dot <aalhikamlearningcenter@gmail.com>
2026-08-03 20:13:44 +0100

    Update telegram_service.py

--- telegram_service.py
+++ telegram_service.py
@@ -1,6 +1,14 @@
 from telegram import Bot
 import os
 from datetime import datetime, timedelta
+
+from config import (
+    MAIN_GROUP_ID,
+    SCIENCE_FACULTY_ID,
+    ARTS_FACULTY_ID,
+    COMMERCIAL_FACULTY_ID,
+)
+
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 bot = Bot(token=BOT_TOKEN)
@@ -11,15 +19,22 @@ async def send_message(chat_id, text):
         chat_id=chat_id,
         text=text
     )
-from config import (
-    MAIN_GROUP_ID,
-    SCIENCE_FACULTY_ID,
-    ARTS_FACULTY_ID,
-    COMMERCIAL_FACULTY_ID,
-)
+
+
 async def create_unique_invite_link():
 
- async def create_faculty_invite_link(faculty):
+    expire_time = datetime.utcnow() + timedelta(minutes=10)
+
+    invite = await bot.create_chat_invite_link(
+        chat_id=MAIN_GROUP_ID,
+        expire_date=expire_time,
+        member_limit=1,
+    )
+
+    return invite.invite_link
+
+
+async def create_faculty_invite_link(faculty):
 
     if faculty == "Science":
         chat_id = SCIENCE_FACULTY_ID
@@ -33,8 +48,6 @@ async def create_unique_invite_link():
     else:
         return None
 
-    from datetime import datetime, timedelta
-
     expire_time = datetime.utcnow() + timedelta(minutes=10)
 
     invite = await bot.create_chat_invite_link(
@@ -43,9 +56,8 @@ async def create_unique_invite_link():
         member_limit=1,
     )
 
-    
-
     return invite.invite_link
+
 
 async def send_welcome_message(chat_id, full_name):
 
