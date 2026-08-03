3a371a134f9ce721485c8bbb82592f7b947539aa
aalhikamlearningcenter-dot <aalhikamlearningcenter@gmail.com>
2026-08-03 17:47:04 +0100

    Update telegram_service.py

--- telegram_service.py
+++ telegram_service.py
@@ -11,8 +11,19 @@ async def send_message(chat_id, text):
         chat_id=chat_id,
         text=text
     )
-async def create_unique_invite_link(chat_id):
-    ...
+from config import MAIN_GROUP_ID
+
+async def create_unique_invite_link():
+
+    expire_time = datetime.utcnow() + timedelta(minutes=10)
+
+    invite = await bot.create_chat_invite_link(
+        chat_id=MAIN_GROUP_ID,
+        expire_date=expire_time,
+        member_limit=1
+    )
+
+    return invite.invite_link
 
 async def send_welcome_message(chat_id, full_name):
 
