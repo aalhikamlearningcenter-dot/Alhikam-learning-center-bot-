b9f7cc1b45d7d1c0dd568ceed77baa823ef96b6e
aalhikamlearningcenter-dot <aalhikamlearningcenter@gmail.com>
2026-08-04 07:02:04 +0100

    Update config.py

--- config.py
+++ config.py
@@ -10,19 +10,55 @@ BOT_USERNAME = "Alhikamcenterbot"
 
 
 # ==========================================
-# TELEGRAM CHANNEL IDS
+# MAIN
 # ==========================================
 
 MAIN_GROUP_ID = -1004384506380
+
 ANNOUNCEMENT_CHANNEL_ID = -1004315707986
 
+
+# ==========================================
+# SCIENCE
+# ==========================================
+
 SCIENCE_FACULTY_ID = -1004479887604
+
 PHYSICS_ID = -1004467391688
+
 CHEMISTRY_ID = -1003575115831
+
 BIOLOGY_ID = -1004412247385
+
 MATHEMATICS_ID = -1004480230539
+
 AGRICULTURAL_SCIENCE_ID = -1004398599335
+
 GEOGRAPHY_ID = -1003901130871
 
+
+# ==========================================
+# ARTS
+# ==========================================
+
 ARTS_FACULTY_ID = -1004314659728
+
+ENGLISH_ID = 0
+LITERATURE_ID = 0
+GOVERNMENT_ID = 0
+ISLAMIC_STUDIES_ID = 0
+HISTORY_ID = 0
+CIVIC_EDUCATION_ID = 0
+
+
+# ==========================================
+# COMMERCIAL
+# ==========================================
+
 COMMERCIAL_FACULTY_ID = -1003967146846
+
+ACCOUNTING_ID = 0
+ECONOMICS_ID = 0
+COMMERCE_ID = 0
+MARKETING_ID = 0
+BUSINESS_STUDIES_ID = 0
